314b1030140781d336dd10eb13ee1c41ca62e0e9
AdiyZd <154992866+AdiyZd@users.noreply.github.com>
2024-09-12 13:41:52 +0700

    Create jembatan.js

---
+++ jembatan.js
@@ -0,0 +1,18 @@
+fetch('https://23de-36-71-85-37.ngrok-free.app/koneksi.php', {  // URL Ngrok
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+        user: 'admin',
+        email: 'admin@example.com',
+        pass: 'password123'
+    })
+})
+    .then(response => response.text())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
